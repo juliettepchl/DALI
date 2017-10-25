24bbe1d3175eef12b59a0b54ebba84e4e5d535a7
Luke Hudspeth <luke.a.hudspeth.19@dartmouth.edu>
2017-10-24 20:24:46 -0400

    Added functionality to play Fragment

--- app/src/main/java/cs65/confuse/PlayFragment.java
+++ app/src/main/java/cs65/confuse/PlayFragment.java
@@ -5,18 +5,33 @@ package cs65.confuse;
  * interactive tab of the sliding tab layout.
  */
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class PlayFragment extends Fragment {
+    private Button playButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.playfragment, container, false);
-    }
 
+        playButton = container.findViewById(R.id.playButton);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (playButton.getText().toString().equals("I ALREADY HAVE AN ACCOUNT")){
+                    Intent myIntent = new Intent(getActivity(), MapActivity.class);
+                    startActivity(myIntent);
+                }
+
+            }
+        });
+    }
 }
